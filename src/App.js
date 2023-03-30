03aed57f186ad540b63851f5cf1a6d865d415b55
ShraddhaBidwe <shraddhabidve123@gmail.com>
2023-03-30 11:51:49 +0530

    Add to cart changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from "./Components/Footer.js";
+import Cart from './Components/Cart.js';
+
+
+
+
+function App() {
+  return (
+    <div className='App'>
+    {/* <Footer/> */}
+    <Cart/>
+    
+    </div>
+  );
+}
+
+export default App;
